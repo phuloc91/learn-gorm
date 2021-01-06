fe88bdb10b5aca899968a4068adb1b7a1e148c3c
Phuloc91 <65276687+phuloc91@users.noreply.github.com>
2021-01-06 23:49:16 +0700

    Update Permission.go

--- models/Permission.go
+++ models/Permission.go
@@ -1,12 +1,12 @@
 package models
 
 type Permission struct {
-	ID     int
+	ID     int 	`gorm:"primarykey"`
 	Name   string  `gorm:"column:NAME"`
 	ZoneID int     `gorm:"column:ZONE_ID"`
 	Status int     `gorm:"column:STATUS"`
-	Roles  []*Role `gorm:"many2many:ROLE_PERMISSION;"`
-	APIS   []*API  `gorm:"many2many:API_PERMISSION;"`
+	Roles  []Role `gorm:"many2many:ROLE_PERMISSION;"`
+	APIS   []API  `gorm:"many2many:API_PERMISSION;"`
 }
 
 func (Permission) TableName() string {
